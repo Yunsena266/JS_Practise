8c04382a3923cabcce797acf95f274c2b97adc90
Aysena Mironova <yunsena26@mail.ru>
2020-11-15 10:23:41 +0300

    TwoTasks

---
+++ TaskAboutLogin/code.js
@@ -0,0 +1,12 @@
+﻿let loggin= prompt("Enter your login,pls");
+if (loggin=="Adm") {
+
+ let pr= prompt("Enter the parole");
+	if (pr=="Я главный") {
+		alert("Здравствуйте");}
+	else if (pr=='' || pr==null) {alert("Cancelled");}
+	else {alert("Неверный пароль");}
+
+ }
+else if (loggin=='' || loggin==null) { alert("Cancelled");}
+else {alert("Ich kenne dich nicht") ;}
