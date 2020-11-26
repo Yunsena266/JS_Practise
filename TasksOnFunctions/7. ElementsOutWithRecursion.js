8f178ad5731ff38fe9c83db7cd35742fdb05aa4e
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-11-26 15:14:00 +0300

    Add files via upload

---
+++ TasksOnFunctions/7. ElementsOutWithRecursion.js
@@ -0,0 +1,10 @@
+let mas = [1,2,3,4,5,6];
+function PrintMas (mas) {
+	mas.slice(0,1);
+	console.log(mas[0]);
+     if ( mas.length > 0) {
+		 return (PrintMas(mas) );
+	 }   
+
+}
+console.log(PrintMas(mas));
