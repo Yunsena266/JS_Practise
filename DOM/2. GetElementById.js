2f83312149a58dc8c90a9f0f2acdd4a54b40f92a
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-12-03 09:10:36 +0300

    Add files via upload
    Task description: Write a code which after a clicking on button change a text inside in input

---
+++ DOM/2. GetElementById.js
@@ -0,0 +1,6 @@
+<input type="text" value="???" id="test">
+<input type="submit" onclick="ChangeText()">
+function ChangeText {
+	let LinkToText = document.getElementById("test");
+	LinkToText.value= "!!!";
+} 
