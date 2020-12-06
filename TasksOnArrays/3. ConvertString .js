99138e2e051366c7b1f5b4d552693ef8a15f73b5
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-12-06 15:09:22 +0300

    Update 3. ConvertString .js
    Task description is added

--- TasksOnArrays/3. ConvertString .js
+++ TasksOnArrays/3. ConvertString .js
@@ -1,3 +1,6 @@
+/*
+Task description: Write a function which convert a string like «my-short-string» to «myShortString».
+*/
 function camelize(str){
 	let Parts = camelize.split('-');
 	return Parts.join();
