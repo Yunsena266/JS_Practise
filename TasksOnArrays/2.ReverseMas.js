7d4e6b529811b643781a5c707fe6dbfb7b13833e
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-12-06 15:07:04 +0300

    Update 2.ReverseMas.js
    Task description is added.

--- TasksOnArrays/2.ReverseMas.js
+++ TasksOnArrays/2.ReverseMas.js
@@ -1,3 +1,6 @@
+/*
+Task description: The array of numbers is given. Reverse it without special method "reverse".
+*/
 let arr = [1,2,3,4,5];
 let ReverseArr = [];
 for (let i= arr.length - 1; i >= 0; i -- ) {
