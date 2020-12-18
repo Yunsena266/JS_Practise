ce37e000627faf9e202aba6aedb3bc336dde86d9
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-12-18 17:30:41 +0300

    Update 3. WhatTheRealNameOfJS.js
    The task description is added

--- TasksOnFunctions/3. WhatTheRealNameOfJS.js
+++ TasksOnFunctions/3. WhatTheRealNameOfJS.js
@@ -1,3 +1,6 @@
+/*
+Write a function which ask a question and accept an answer. If the answer is right "Right" is printed else "Wrong"
+*/
 ﻿let answ=prompt("What's the name of JS?");
 	let answw= (answ=="ECMAScript") ? ("Right!") : ("WRONG");
 	alert(answw);
