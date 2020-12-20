20b5f3a40541f99fab7acf82c338290cd0296bad
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2020-12-20 09:19:08 +0300

    Update 2. GetElementById.js
    The task description is added.

--- DOM/2. GetElementById.js
+++ DOM/2. GetElementById.js
@@ -1,3 +1,6 @@
+/*
+Write a function ChangeText which after a clicking a button by user changes a text.
+*/
 <input type="text" value="???" id="test">
 <input type="submit" onclick="ChangeText()">
 function ChangeText {
