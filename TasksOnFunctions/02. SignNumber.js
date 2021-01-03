a0af906ebb349ed6c893b6019e76b721a441d4c9
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2021-01-03 18:07:05 +0300

    Update and rename 2. SignNumber.js to 02. SignNumber.js
    1) The sequential number is changed. 
    2) The task description is given

---
+++ TasksOnFunctions/02. SignNumber.js
@@ -0,0 +1,6 @@
+﻿/*
+ Write a code which accept a number and return it sign.
+ */
+let answ=prompt("Enter the number, pls","0");
+let answw= (answ>0) ? 'Positive':  (answ<0) ? 'Negative':(answ==0) ? '0': 'Zero';
+alert(answw);
--- TasksOnFunctions/2. SignNumber.js
+++
@@ -1,3 +0,0 @@
-﻿let answ=prompt("Enter the number, pls","0");
-let answw= (answ>0) ? 'Positive':  (answ<0) ? 'Negative':(answ==0) ? '0': 'Zero';
-alert(answw);
