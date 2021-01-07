110e63657fbb1ff07e9856188629d53059302f78
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2021-01-07 17:59:31 +0300

    Add files via upload
    The sixteenth task

---
+++ TasksOnFunctions/16. SumByLoopback.js
@@ -0,0 +1,20 @@
+/*
+Write a function sum, which works in that way: sum(a)(b) = a+b.
+
+For example:
+
+
+sum(1)(2) = 3
+sum(5)(-1) = 4
+
+
+*/
+
+
+function sum (a)  {
+	return function (b) {
+            return a + b
+   }
+}
+
+
