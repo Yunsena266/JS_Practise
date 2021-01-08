bc62b36b0434bd3e0e0b0b357f1270f78be6db8d
Айсена Миронова <45133269+Yunsena266@users.noreply.github.com>
2021-01-08 12:17:44 +0300

    Add files via upload
    The 17th task

---
+++ TasksOnFunctions/17. FilterByFunctions.js
@@ -0,0 +1,28 @@
+/*
+There is built-in method arr.filter(f) for arrays. 
+It filters all elements with functions f. If it returns true, then element will added to returning array.
+
+Write a group of "ready for using" filters:
+
+inBetween(a, b) –between a и b (including).
+inArray([...]) – existed in this array.
+
+*/
+
+
+function inBetween(a, b) {
+	return function (item) {
+		return ( item <= b & item >= a) 
+	}
+	
+};
+
+function inArray([...]) {
+	let ar = [...];
+	return( ar.includes(item)) 
+	
+};
+
+let arr = [1, 2, 3, 4, 5];
+console.log( arr.filter(inArray([1, 2, 12])) );
+console.log( arr.filter(inArray([1, 2, 12])) );
